1bda85a8867347a7b6c8927772e93705f16003a3
Otavio Oshiro <otavioshiro@msn.com>
2020-08-12 23:49:44 -0400

    Aula 107 com erro e pergunta na Udemy

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -67,6 +67,16 @@ class User extends Model {
 
 		$_SESSION[User::SESSION] = NULL;
 	}
+
+	public static function listAll(){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+	}
+
+	
+
 }
 
  ?>
