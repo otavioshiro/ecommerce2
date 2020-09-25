490790400b54899b0a78c67d6e7e1bc9d6fdf594
Otávio Oshiro <oshiroliveira@gmail.com>
2020-09-25 19:53:23 -0400

    Aulas 113 - 114 com problemas no template

--- views-cache/category.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/category.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -16,21 +16,22 @@
         <div class="row">
             <?php $counter1=-1;  if( isset($products) && ( is_array($products) || $products instanceof Traversable ) && sizeof($products) ) foreach( $products as $key1 => $value1 ){ $counter1++; ?>
 
-            <div class="col-md-3 col-sm-6">
-                <div class="single-shop-product">
-                    <div class="product-upper">
-                        <img src="/res/site/img/product-2.jpg" alt="">
-                    </div>
-                    <h2><a href="">Apple new mac book 2015 March :P</a></h2>
-                    <div class="product-carousel-price">
-                        <ins>$899.00</ins> <del>$999.00</del>
-                    </div>  
+    <div class="col-md-3 col-sm-6">
+        <div class="single-shop-product">
+            <div class="product-upper">
+                <img src="<?php echo htmlspecialchars( $value1["desphoto"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" alt="">
+            </div>
+            <h2><a href="/products/<?php echo htmlspecialchars( $value1["desurl"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["desproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?></a></h2>
+            <div class="product-carousel-price">
+                <ins>R$<?php echo formatPrice($value1["vlprice"]); ?></ins> 
+            </div>  
                     
-                    <div class="product-option-shop">
-                        <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="70" rel="nofollow" href="/canvas/shop/?add-to-cart=70">Add to cart</a>
-                    </div>                       
-                </div>
-            <?php } ?>
+            <div class="product-option-shop">
+                <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="70" rel="nofollow" href="/canvas/shop/?add-to-cart=70">Comprar</a>
+            </div>                       
+        </div>
+    </div>
+        <?php } ?>
 
             </div>
         
@@ -39,21 +40,10 @@
                 <div class="product-pagination text-center">
                     <nav>
                         <ul class="pagination">
-                        <li>
-                            <a href="#" aria-label="Previous">
-                            <span aria-hidden="true">«</span>
-                            </a>
-                        </li>
-                        <li><a href="#">1</a></li>
-                        <li><a href="#">2</a></li>
-                        <li><a href="#">3</a></li>
-                        <li><a href="#">4</a></li>
-                        <li><a href="#">5</a></li>
-                        <li>
-                            <a href="#" aria-label="Next">
-                            <span aria-hidden="true">»</span>
-                            </a>
-                        </li>
+                        {loop=$pages}
+                        <li><a href="<?php echo htmlspecialchars( $value["link"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value["page"], ENT_COMPAT, 'UTF-8', FALSE ); ?></a></li>
+                        <?php } ?>
+
                         </ul>
                     </nav>                        
                 </div>
