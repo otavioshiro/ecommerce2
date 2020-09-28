d8995d3232dfb859371c08f791a81115c0aa7ae7
Otávio Oshiro <oshiroliveira@gmail.com>
2020-09-28 09:21:16 -0400

    Correção do  template - Aula 114

--- views-cache/category.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/category.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -40,8 +40,9 @@
                 <div class="product-pagination text-center">
                     <nav>
                         <ul class="pagination">
-                        {loop=$pages}
-                        <li><a href="<?php echo htmlspecialchars( $value["link"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value["page"], ENT_COMPAT, 'UTF-8', FALSE ); ?></a></li>
+                        <?php $counter1=-1;  if( isset($pages) && ( is_array($pages) || $pages instanceof Traversable ) && sizeof($pages) ) foreach( $pages as $key1 => $value1 ){ $counter1++; ?>
+
+                        <li><a href="<?php echo htmlspecialchars( $value1["link"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["page"], ENT_COMPAT, 'UTF-8', FALSE ); ?></a></li>
                         <?php } ?>
 
                         </ul>
