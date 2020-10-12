994b8283881765774a0944a6d0d651efd14166ff
Otávio Oshiro <oshiroliveira@gmail.com>
2020-10-12 17:14:00 -0400

    Aula 117 concluída com sucesso
    Ainda continua o erro da parte de edição de produtos

--- views-cache/cart.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/cart.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -58,7 +58,7 @@
 
                                         <td class="product-quantity">
                                             <div class="quantity buttons_added">
-                                                <input type="button" class="minus" value="-" onclick="window.location.href = '/cart/{$value.idproduct'">
+                                                <input type="button" class="minus" value="-" onclick="window.location.href = '/cart/<?php echo htmlspecialchars( $value1["idproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?>/minus'">
                                                 <input type="number" size="4" class="input-text qty text" title="Qty" value="<?php echo htmlspecialchars( $value1["nrqtd"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" min="0" step="1">
                                                 <input type="button" class="plus" value="+" onclick="window.location.href = '/cart/<?php echo htmlspecialchars( $value1["idproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?>/add'">
                                             </div>
