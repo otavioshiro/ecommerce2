4ecc5f9afeb63d65ea89db0c6eabb43304c6e92e
Otávio Oshiro <oshiroliveira@gmail.com>
2020-10-21 16:19:10 -0400

    Update Mailer.php

--- vendor/hcodebr/php-classes/src/Mailer.php
+++ vendor/hcodebr/php-classes/src/Mailer.php
@@ -9,8 +9,8 @@ use Rain\Tpl;
 
 class Mailer{
 
-	const USERNAME = "oshiroliveira@gmail.com";
-	const PASSWORD = "visconde.01"; /* "<?password?>"; */
+	const USERNAME = "@gmail.com";
+	const PASSWORD = ""; /* "<?password?>"; */
 	const NAME_FROM = "Hcode Store";
 
 	private $mail;
